f2319d42469cc6d9fc67930560284e2333b5304c
Nissaaprilia <nissa.2904az@gmail.com>
2023-03-16 16:03:47 +0800

    materi13

--- javascript/penjumlahanberlatih.js
+++ javascript/penjumlahanberlatih.js
@@ -1,27 +1,27 @@
-//cek jawaban isian
+//cek jawaban materi 13
 function cekJawaban() {
-  let jawaban16 = document.querySelectorAll("#isian1");
-  let jawabanbenar = ["18", "20"];
+  let jawabanmateri13 = document.querySelectorAll("#isian1");
+  let jawabanbenar = ["18", "20", "3", "8", "6", "7", "8", "9", "5", "6", "9", "14", "9", "12", "10", "15", "7", "12", "15", "17"];
 
-  for (let i = 0; i < jawaban16.length; i++) {
+  for (let i = 0; i < jawabanmateri13.length; i++) {
     //length ada 12 jawaban
-    if (jawaban16[i].value.toLowerCase() == jawabanbenar[i]) {
-      jawaban16[i].style.border = "2px solid green";
+    if (jawabanmateri13[i].value.toLowerCase() == jawabanbenar[i]) {
+      jawabanmateri13[i].style.border = "2px solid green";
       // hasilJawaban[i].innerHTML = "Benar";
       // hasilJawaban[i].style.color = "green";
     } else {
-      jawaban16[i].style.border = "2px solid red";
+      jawabanmateri13[i].style.border = "2px solid red";
       // hasilJawaban[i].innerHTML = "Salah";
       // hasilJawaban[i].style.color = "green";
     }
   }
 }
-//reset isian
+//reset jawaban materi 13
 function reset() {
-  let jawaban16 = document.querySelectorAll("#isian1");
-  for (let i = 0; i < jawaban16.length; i++) {
-    jawaban16[i].value = "";
-    jawaban16[i].style.border = "0.5px solid black";
-    jawaban16[i].ariaPlaceholder = ". . .";
+  let jawabanmateri13 = document.querySelectorAll("#isian1");
+  for (let i = 0; i < jawabanmateri13.length; i++) {
+    jawabanmateri13[i].value = "";
+    jawabanmateri13[i].style.border = "0.5px solid black";
+    jawabanmateri13[i].ariaPlaceholder = ". . .";
   }
 }
