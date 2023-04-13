560be5f1a6020c9790981b3ba3ecbb1ea99b15d5
Nissaaprilia <nissa.2904az@gmail.com>
2023-04-13 12:11:12 +0800

    register

--- javascript/login.js
+++ javascript/login.js
@@ -45,3 +45,21 @@ function signout() {
 }
 
 console.log(localStorage);
+
+
+    <!-- function Signout() {
+        // signoutlink.href = '../index.html';
+        sessionStorage.removeItem("nama");
+        window.location = "../../index.html";
+      }
+
+      // window Load
+
+      window.onload = function () {
+        getName();
+        if (currentUser == null) {
+          signoutlink.href = "../../index.html";
+        } else {
+          userlink.innerText = currentUser.nama;
+          signoutlink.innerText = "Sign Out";
+        } -->
