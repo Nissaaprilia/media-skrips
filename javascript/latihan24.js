3fc66cd4ea3f633c6e6750526505472bbf9ae076
Nissaaprilia <nissa.2904az@gmail.com>
2023-05-03 16:39:10 +0800

    .

--- javascript/latihan24.js
+++ javascript/latihan24.js
@@ -61,20 +61,20 @@ document.getElementById("btnLanjut6").addEventListener("click", function () {
   document.getElementById("soal6").style.display = "none";
 });
 
-// document.getElementById("btnLanjut7").addEventListener("click", function () {
-//   document.getElementById("soal8").style.display = "block";
-//   document.getElementById("soal7").style.display = "none";
-// });
+document.getElementById("btnLanjut7").addEventListener("click", function () {
+  document.getElementById("soal8").style.display = "block";
+  document.getElementById("soal7").style.display = "none";
+});
 
-// document.getElementById("btnLanjut8").addEventListener("click", function () {
-//   document.getElementById("soal9").style.display = "block";
-//   document.getElementById("soal8").style.display = "none";
-// });
+document.getElementById("btnLanjut8").addEventListener("click", function () {
+  document.getElementById("soal9").style.display = "block";
+  document.getElementById("soal8").style.display = "none";
+});
 
-// document.getElementById("btnLanjut9").addEventListener("click", function () {
-//   document.getElementById("soal10").style.display = "block";
-//   document.getElementById("soal9").style.display = "none";
-// });
+document.getElementById("btnLanjut9").addEventListener("click", function () {
+  document.getElementById("soal10").style.display = "block";
+  document.getElementById("soal9").style.display = "none";
+});
 
 var jwb1 = document.getElementById("jwb1");
 var jwb11 = document.getElementById("jwb11");
@@ -83,7 +83,7 @@ var jwb2 = document.getElementById("jwb2");
 var jwb21 = document.getElementById("jwb21");
 var jwb22 = document.getElementById("jwb22");
 var jwb3 = document.getElementById("jwb3");
-var jwb31 = document.getElementById("jwb21");
+var jwb31 = document.getElementById("jwb31");
 var jwb32 = document.getElementById("jwb32");
 var jwb4 = document.getElementById("jwb4");
 var jwb41 = document.getElementById("jwb41");
@@ -109,7 +109,7 @@ var jwb102 = document.getElementById("jwb102");
 var a = [];
 
 document.getElementById("btnCek1")?.addEventListener("click", function () {
-  if (jwb1.value == 11 && jwb11.value == 6 && jwb12.value == 6) {
+  if (jwb1.value == 3 && jwb11.value == 1 && jwb12.value == 3) {
     jwb1.classList.add("jawabpecahanbenar");
     jwb1.classList.remove("jawabpecahansalah");
     jwb11.classList.add("jawabpecahanbenar");
@@ -139,14 +139,14 @@ document.getElementById("btnCek1")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek2")?.addEventListener("click", function () {
-  if (jwb2.value == 2 && jwb21.value == 6 && jwb22.value == 6) {
+  if (jwb2.value == 3 && jwb21.value == 5 && jwb22.value == 6) {
     jwb2.classList.add("jawabpecahanbenar");
     jwb2.classList.remove("jawabpecahansalah");
     jwb21.classList.add("jawabpecahanbenar");
     jwb21.classList.remove("jawabpecahansalah");
     jwb22.classList.add("jawabpecahanbenar");
     jwb22.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut2").style.display = "block";
     document.getElementById("btnCek2").style.display = "none";
     document.getElementById("alertHasil2").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -160,7 +160,7 @@ document.getElementById("btnCek2")?.addEventListener("click", function () {
     jwb21.classList.remove("jawabpecahanbenar");
     jwb22.classList.add("jawabpecahansalah");
     jwb22.classList.remove("jawabpecahanbenar");
-    document.getElementById("alertHasil1").innerHTML = `
+    document.getElementById("alertHasil2").innerHTML = `
       <div class="alert alert-danger" role="alert">Jawaban salah. <br> 
      Pecahan mempunyai penyebut berbeda, maka ubah lah menjadi pecahan senilai sehingga penyebut nya sama.
 </div>
@@ -169,14 +169,14 @@ document.getElementById("btnCek2")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek3")?.addEventListener("click", function () {
-  if (jwb3.value == 3 && jwb31.value == 6 && jwb32.value == 6) {
+  if (jwb3.value == 3 && jwb31.value == 2 && jwb32.value == 3) {
     jwb3.classList.add("jawabpecahanbenar");
     jwb3.classList.remove("jawabpecahansalah");
     jwb31.classList.add("jawabpecahanbenar");
     jwb31.classList.remove("jawabpecahansalah");
     jwb32.classList.add("jawabpecahanbenar");
     jwb32.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut3").style.display = "block";
     document.getElementById("btnCek3").style.display = "none";
     document.getElementById("alertHasil3").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -190,7 +190,7 @@ document.getElementById("btnCek3")?.addEventListener("click", function () {
     jwb31.classList.remove("jawabpecahanbenar");
     jwb32.classList.add("jawabpecahansalah");
     jwb32.classList.remove("jawabpecahanbenar");
-    document.getElementById("alertHasil1").innerHTML = `
+    document.getElementById("alertHasil3").innerHTML = `
       <div class="alert alert-danger" role="alert">Jawaban salah. <br> 
      Pecahan mempunyai penyebut berbeda, maka ubah lah menjadi pecahan senilai sehingga penyebut nya sama.
 </div>
@@ -199,14 +199,14 @@ document.getElementById("btnCek3")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek4")?.addEventListener("click", function () {
-  if (jwb4.value == 4 && jwb41.value == 6 && jwb42.value == 6) {
+  if (jwb4.value == 7 && jwb41.value == 7 && jwb42.value == 15) {
     jwb4.classList.add("jawabpecahanbenar");
     jwb4.classList.remove("jawabpecahansalah");
     jwb41.classList.add("jawabpecahanbenar");
     jwb41.classList.remove("jawabpecahansalah");
     jwb42.classList.add("jawabpecahanbenar");
     jwb42.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut4").style.display = "block";
     document.getElementById("btnCek4").style.display = "none";
     document.getElementById("alertHasil4").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -229,14 +229,14 @@ document.getElementById("btnCek4")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek5")?.addEventListener("click", function () {
-  if (jwb5.value == 5 && jwb51.value == 6 && jwb52.value == 6) {
+  if (jwb5.value == 5 && jwb51.value == 5 && jwb52.value == 6) {
     jwb5.classList.add("jawabpecahanbenar");
     jwb5.classList.remove("jawabpecahansalah");
     jwb51.classList.add("jawabpecahanbenar");
     jwb51.classList.remove("jawabpecahansalah");
     jwb52.classList.add("jawabpecahanbenar");
     jwb52.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut5").style.display = "block";
     document.getElementById("btnCek5").style.display = "none";
     document.getElementById("alertHasil5").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -259,14 +259,14 @@ document.getElementById("btnCek5")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek6")?.addEventListener("click", function () {
-  if (jwb6.value == 6 && jwb61.value == 6 && jwb62.value == 6) {
+  if (jwb6.value == 7 && jwb61.value == 1 && jwb62.value == 12) {
     jwb6.classList.add("jawabpecahanbenar");
     jwb6.classList.remove("jawabpecahansalah");
     jwb61.classList.add("jawabpecahanbenar");
     jwb61.classList.remove("jawabpecahansalah");
     jwb62.classList.add("jawabpecahanbenar");
     jwb62.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut6").style.display = "block";
     document.getElementById("btnCek6").style.display = "none";
     document.getElementById("alertHasil6").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -289,14 +289,14 @@ document.getElementById("btnCek6")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek7")?.addEventListener("click", function () {
-  if (jwb7.value == 7 && jwb71.value == 7 && jwb72.value == 7) {
+  if (jwb7.value == 10 && jwb71.value == 2 && jwb72.value == 15) {
     jwb7.classList.add("jawabpecahanbenar");
     jwb7.classList.remove("jawabpecahansalah");
     jwb71.classList.add("jawabpecahanbenar");
     jwb71.classList.remove("jawabpecahansalah");
     jwb72.classList.add("jawabpecahanbenar");
     jwb72.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut7").style.display = "block";
     document.getElementById("btnCek7").style.display = "none";
     document.getElementById("alertHasil7").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -319,14 +319,14 @@ document.getElementById("btnCek7")?.addEventListener("click", function () {
 });
 
 document.getElementById("btnCek8")?.addEventListener("click", function () {
-  if (jwb8.value == 8 && jwb81.value == 8 && jwb82.value == 8) {
+  if (jwb8.value == 7 && jwb81.value == 5 && jwb82.value == 12) {
     jwb8.classList.add("jawabpecahanbenar");
     jwb8.classList.remove("jawabpecahansalah");
     jwb81.classList.add("jawabpecahanbenar");
     jwb81.classList.remove("jawabpecahansalah");
     jwb82.classList.add("jawabpecahanbenar");
     jwb82.classList.remove("jawabpecahansalah");
-    document.getElementById("btnLanjut").style.display = "block";
+    document.getElementById("btnLanjut8").style.display = "block";
     document.getElementById("btnCek8").style.display = "none";
     document.getElementById("alertHasil8").innerHTML = `
         <div class="alert alert-info" role="alert">
@@ -341,6 +341,66 @@ document.getElementById("btnCek8")?.addEventListener("click", function () {
     jwb82.classList.add("jawabpecahansalah");
     jwb82.classList.remove("jawabpecahanbenar");
     document.getElementById("alertHasil8").innerHTML = `
+      <div class="alert alert-danger" role="alert">Jawaban salah. <br> 
+     Pecahan mempunyai penyebut berbeda, maka ubah lah menjadi pecahan senilai sehingga penyebut nya sama.
+</div>
+      `;
+  }
+});
+
+document.getElementById("btnCek9")?.addEventListener("click", function () {
+  if (jwb9.value == 3 && jwb91.value == 5 && jwb92.value == 6) {
+    jwb9.classList.add("jawabpecahanbenar");
+    jwb9.classList.remove("jawabpecahansalah");
+    jwb91.classList.add("jawabpecahanbenar");
+    jwb91.classList.remove("jawabpecahansalah");
+    jwb92.classList.add("jawabpecahanbenar");
+    jwb92.classList.remove("jawabpecahansalah");
+    document.getElementById("btnLanjut9").style.display = "block";
+    document.getElementById("btnCek9").style.display = "none";
+    document.getElementById("alertHasil9").innerHTML = `
+        <div class="alert alert-info" role="alert">
+            Jawaban benar. Klik soal berikutnya!
+        </div>
+      `;
+  } else {
+    jwb9.classList.add("jawabpecahansalah");
+    jwb9.classList.remove("jawabpecahanbenar");
+    jwb91.classList.add("jawabpecahansalah");
+    jwb91.classList.remove("jawabpecahanbenar");
+    jwb92.classList.add("jawabpecahansalah");
+    jwb92.classList.remove("jawabpecahanbenar");
+    document.getElementById("alertHasil9").innerHTML = `
+      <div class="alert alert-danger" role="alert">Jawaban salah. <br> 
+     Pecahan mempunyai penyebut berbeda, maka ubah lah menjadi pecahan senilai sehingga penyebut nya sama.
+</div>
+      `;
+  }
+});
+
+document.getElementById("btnCek10")?.addEventListener("click", function () {
+  if (jwb10.value == 3 && jwb101.value == 3 && jwb102.value == 4) {
+    jwb10.classList.add("jawabpecahanbenar");
+    jwb10.classList.remove("jawabpecahansalah");
+    jwb101.classList.add("jawabpecahanbenar");
+    jwb101.classList.remove("jawabpecahansalah");
+    jwb102.classList.add("jawabpecahanbenar");
+    jwb102.classList.remove("jawabpecahansalah");
+    document.getElementById("btnLanjut10").style.display = "block";
+    document.getElementById("btnCek10").style.display = "none";
+    document.getElementById("alertHasil10").innerHTML = `
+        <div class="alert alert-info" role="alert">
+            Jawaban benar. Kamu bisa lanjut ke materi berikutnya!
+        </div>
+      `;
+  } else {
+    jwb10.classList.add("jawabpecahansalah");
+    jwb10.classList.remove("jawabpecahanbenar");
+    jwb101.classList.add("jawabpecahansalah");
+    jwb101.classList.remove("jawabpecahanbenar");
+    jwb102.classList.add("jawabpecahansalah");
+    jwb102.classList.remove("jawabpecahanbenar");
+    document.getElementById("alertHasil10").innerHTML = `
       <div class="alert alert-danger" role="alert">Jawaban salah. <br> 
      Pecahan mempunyai penyebut berbeda, maka ubah lah menjadi pecahan senilai sehingga penyebut nya sama.
 </div>
