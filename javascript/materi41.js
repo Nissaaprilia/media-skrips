7bff8b6a15c9bc5f4e66369bceee48a30b130ef8
Nissaaprilia <nissa.2904az@gmail.com>
2023-05-03 12:28:28 +0800

    sisa soal pecahan campuran

--- javascript/materi41.js
+++ javascript/materi41.js
@@ -31,6 +31,32 @@ function LogOut() {
   window.location = "/index.html";
 }
 
+// Function;
+function getNama() {
+  if (nama == null) {
+    window.location = "/index.html";
+  } else {
+    userlink.innerText = nama;
+  }
+}
+
+function getNisn() {
+  if (nisn == null) {
+    window.location = "/index.html";
+  } else {
+    nisnlink.innerText = nisn;
+  }
+}
+
+getNama();
+getNisn();
+
+function LogOut() {
+  sessionStorage.removeItem("nama");
+
+  window.location = "/index.html";
+}
+
 //Contoh Soal
 let kenya = document.querySelectorAll(".ke");
 let langkahnya = document.querySelectorAll(".langkahnya");
@@ -49,44 +75,136 @@ for (let i = 0; i < kenya.length; i++) {
   });
 }
 
-// tahapan contoh soal
-let bgnya = document.querySelectorAll(".pjm");
-let bgnya1 = document.querySelectorAll(".krng");
-let jadi = document.querySelectorAll(".aau");
-let tmblnya = document.querySelector(".tmb_pnjumlahan");
-let tmblnya1 = document.querySelector(".tmb_pngrangan");
-let kondisi1 = 0;
-let kondisi2 = 0;
-tmblnya.addEventListener("click", function () {
-  if (kondisi1 == 0) {
-    kondisi1 += 1;
-    bgnya[0].className = bgnya[0].className.replace("hilang1", "");
-    tmblnya.innerText = "Ubahlah menjadi pecahan senilai";
-  } else if (kondisi1 == 1) {
-    kondisi1 += 1;
-    bgnya[1].className = bgnya[1].className.replace("hilang1", "");
-    bgnya[2].className = bgnya[2].className.replace("hilang1", "");
-    tmblnya.innerText = "hasil";
-  } else if (kondisi1 == 2) {
-    bgnya[3].className = bgnya[3].className.replace("hilang1", "");
-    tmblnya.className += " hilang1";
-    jadi[1].className = jadi[1].className.replace("hilang1", "");
-  }
-});
+function diketahui0(diketahui0) {
+  var x = document.getElementById(diketahui0);
 
-tmblnya1.addEventListener("click", function () {
-  if (kondisi2 == 0) {
-    kondisi2 += 1;
-    bgnya1[0].className = bgnya1[0].className.replace("hilang1", "");
-    tmblnya1.innerText = "Ubah menjadi pecahan senilai";
-  } else if (kondisi2 == 1) {
-    kondisi2 += 1;
-    bgnya1[1].className = bgnya1[1].className.replace("hilang1", "");
-    tmblnya1.innerText = "Hasil";
-  } else if (kondisi2 == 2) {
-    bgnya1[2].className = bgnya1[2].className.replace("hilang1", "");
-    bgnya1[3].className = bgnya1[3].className.replace("hilang1", "");
-    tmblnya1.className += " hilang1";
-    jadi[3].className = jadi[3].className.replace("hilang1", "");
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
   }
-});
+}
+
+function ditanya0(ditanya0) {
+  var x = document.getElementById(ditanya0);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+function rumus0(rumus0) {
+  var x = document.getElementById(rumus0);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+
+function diketahui(diketahui) {
+  var x = document.getElementById(diketahui);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+
+function ditanya(ditanya) {
+  var x = document.getElementById(ditanya);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+function rumus(rumus) {
+  var x = document.getElementById(rumus);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+
+function diketahui2(diketahui2) {
+  var x = document.getElementById(diketahui2);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+
+function ditanya2(ditanya2) {
+  var x = document.getElementById(ditanya2);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+function rumus2(rumus2) {
+  var x = document.getElementById(rumus2);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+function rumus22(rumus22) {
+  var x = document.getElementById(rumus22);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+
+function diketahui4(diketahui4) {
+  var x = document.getElementById(diketahui4);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+
+function ditanya4(ditanya4) {
+  var x = document.getElementById(ditanya4);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+function rumus4(rumus4) {
+  var x = document.getElementById(rumus4);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
+function rumus41(rumus41) {
+  var x = document.getElementById(rumus41);
+
+  if (x.className.indexOf("lost") == -1) {
+    x.className += " lost";
+  } else {
+    x.className = x.className.replace("lost", "");
+  }
+}
